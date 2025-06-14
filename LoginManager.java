e294a1e1940a03a467c1542f759687f0427515d9
shrutirai1802 <raishruti2004@gmail.com>
2025-06-14 18:24:53 +0530

    Add files via upload

---
+++ LoginManager.java
@@ -0,0 +1,23 @@
+package manager;
+import java.util.ArrayList;
+import model.*;
+import model.User;
+
+
+public class LoginManager {
+
+    public User login(String u,String p,ArrayList<User> users)
+    {
+        for(User user:users)
+        {
+            String tempUserName=user.getusername();
+            String tempPassword=user.getpassword();
+            if(tempUserName.equals(u) && tempPassword.equals(p))
+            {
+                return user;
+            }
+        }
+        return null;
+    }
+    
+}
